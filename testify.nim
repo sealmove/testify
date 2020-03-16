1bd1a1b0529c4469a3267122d55206d99d2bd539
Stefanos Mandalas <sealmove@protonmail.com>
2020-03-16 03:41:12 +0100

    implemented stdout

--- testify.nim
+++ testify.nim
@@ -1,4 +1,11 @@
-import os, streams, xmltree, strformat
+import os, streams, xmltree, strformat, strutils
+
+const
+  R = "\e[31;1m"
+  G = "\e[32;1m"
+  Y = "\e[33;1m"
+  B = "\e[34;1m"
+  D = "\e[0m"
 
 if paramCount() < 1:
   echo "Too few arguments"
@@ -13,8 +20,10 @@ for d in commandLineParams()[1..^1]:
     suitename = lastPathPart(d)
     testsuite = newElement("testsuite")
     tests: int
-    errors: int
     failures: int
+    errors: int
+
+  stdout.write &"{B}[Suite]{D} " & suitename & "\n"
 
   for f in walkFiles(d / "t*.nim"):
     inc(tests)
@@ -26,6 +35,7 @@ for d in commandLineParams()[1..^1]:
     let c = execShellCmd(&"nim c --outdir:{binDir} {f} >/dev/null 2>&1")
     if c != 0:
       inc(errors)
+      stdout.write &"  {R}[ER]{D} " & casename & "\n"
       testcase.add(newXmlTree("failure", [],
                               {"message": "compile error"}.toXmlAttributes))
     else:
@@ -33,16 +43,24 @@ for d in commandLineParams()[1..^1]:
         chop = splitFile(f)
         exe = chop.dir / "bin" / chop.name
         r = execShellCmd(&"{exe} >/dev/null 2>&1")
-      echo &"{exe} >/dev/null 2>&1"
       if r != 0:
+        stdout.write &"  {Y}[FL]{D} " & casename & "\n"
         inc(failures)
         testcase.add(newElement("failure"))
+      else:
+        stdout.write &"  {G}[OK]{D} " & casename & "\n"
     testsuite.add(testcase)
   testsuite.attrs = {"name": suitename,
                      "tests": $tests,
                      "errors": $errors,
                      "failures": $failures}.toXmlAttributes
   testsuites.add(testsuite)
+
+  echo "----------------------------------------\n" &
+       &"  {G}[OK]{D} {(tests - errors - failures).intToStr(3)}\n" &
+       &"  {Y}[FL]{D} {failures.intToStr(3)}\n" &
+       &"  {R}[ER]{D} {errors.intToStr(3)}\n" &
+       "----------------------------------------\n"
 
 report.write($testsuites)
 close(report)
